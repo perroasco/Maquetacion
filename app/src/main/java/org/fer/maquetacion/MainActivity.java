3866927a7c909c9942144019c92cff0addfcc661
gloom_f_er <fernoglez@gmail.com>
2019-10-14 20:21:53 -0500

    2

--- app/src/main/java/org/fer/maquetacion/MainActivity.java
+++ app/src/main/java/org/fer/maquetacion/MainActivity.java
@@ -2,7 +2,10 @@ package org.fer.maquetacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.fbboton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),Error.class);
+
+                startActivityForResult(intent,0);
+            }
+        });
+
+        Button button2 = findViewById(R.id.gboton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),Error.class);
+
+                startActivityForResult(intent,0);
+            }
+        });
     }
 }
